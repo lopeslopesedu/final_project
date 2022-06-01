08f27a6466a41e19e54c0fb4190565dc7ee9e368
Eduardo Lopes <lopes.lopes.edu@gmail.com>
2022-06-01 17:00:00 -0300

    - Rouge.

--- chatbot.py
+++ chatbot.py
@@ -17,7 +17,7 @@ MAX_LENGTH = 20  # Maximum sentence length to consider
 
 from nltk.translate import bleu_score
 from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
-
+import rouge
 
 class Chatbot:
     def __init__(self):
@@ -48,6 +48,7 @@ class Chatbot:
         self.avg_pontuacao_bleu = 0
         self.pontuacao_bleu = []
         self.resultados = []
+        self.resultado_computar_metricas = []
 
 
     #Métodos privados de treinamento
@@ -205,6 +206,7 @@ class Chatbot:
         input_sentence = ''
         tamanho_base = len(base_testes)
         self.resultados = []
+        self.resultado_computar_metricas = []
         for i in tqdm(range(tamanho_base)):
             try:
                 #pergunta
@@ -221,6 +223,8 @@ class Chatbot:
                 self.resultados.append('T.I.A: ' + resposta)
                 self.resultados.append("Esperada: "+base_testes[i][1])
 
+                self.resultado_computar_metricas.append(base_testes[i][1])
+                self.resultado_computar_metricas.append(resposta)
             except KeyError:
                 pass
                 #print("Error: Encountered unknown word.")
@@ -233,10 +237,70 @@ class Chatbot:
         smooth = SmoothingFunction()
         return sentence_bleu([reference], candidate, smoothing_function=smooth.method2)
 
-
-
     def __calcular_pontuacao_bleu(self,candidata, referencia):
         return sentence_bleu(candidata, referencia,smoothing_function=bleu_score.SmoothingFunction(epsilon=1e-12).method2)
+
+    def calcular_media_metrica(self,resultado_metrica):
+        p = 0
+        r = 0
+        f = 0
+        for i in resultado_metrica:
+            p += i[0]
+            r += i[1]
+            f += i[2]
+        return p/len(resultado_metrica),r/len(resultado_metrica),f/len(resultado_metrica)
+
+    def prepare_results(self,metric,p, r, f):
+        return '\t{}:\t{}: {:5.2f}\t{}: {:5.2f}\t{}: {:5.2f}'.format(metric, 'P', 100.0 * p, 'R', 100.0 * r, 'F1',
+                                                                     100.0 * f)
+    def calcular_rouge(self):
+        all_references =[]
+        all_hypothesis = []
+        resultado_metrica=[]
+        i = 0
+        while i < len(self.resultado_computar_metricas):
+            all_hypothesis.append(self.resultado_computar_metricas[i])
+            all_references.append(self.resultado_computar_metricas[i+1])
+            i += 2
+
+        for aggregator in ['Avg', 'Best', 'Individual']:
+            print('Evaluation with {}'.format(aggregator))
+            apply_avg = aggregator == 'Avg'
+            apply_best = aggregator == 'Best'
+
+            evaluator = rouge.Rouge(metrics=['rouge-n', 'rouge-l', 'rouge-w'],
+                                    max_n=4,
+                                    limit_length=True,
+                                    length_limit=100,
+                                    length_limit_type='words',
+                                    apply_avg=apply_avg,
+                                    apply_best=apply_best,
+                                    alpha=0.5,  # Default F1_score
+                                    weight_factor=1.2,
+                                    stemming=True)
+
+            scores = evaluator.get_scores(all_hypothesis, all_references)
+
+            for metric, results in sorted(scores.items(), key=lambda x: x[0]):
+                resultado_metrica = []
+                if not apply_avg and not apply_best:  # value is a type of list as we evaluate each summary vs each reference
+                    for hypothesis_id, results_per_ref in enumerate(results):
+                        nb_references = len(results_per_ref['p'])
+                        for reference_id in range(nb_references):
+                            #print('\tHypothesis #{} & Reference #{}: '.format(hypothesis_id, reference_id))
+                            #print('\t' + self.prepare_results(metric,results_per_ref['p'][reference_id],
+                            #                             results_per_ref['r'][reference_id],
+                            #                             results_per_ref['f'][reference_id]))
+                            resultado_metrica.append([results_per_ref['p'][reference_id],
+
+                                                      results_per_ref['r'][reference_id],
+                                                      results_per_ref['f'][reference_id]])
+                    media_p,media_r,media_f = self.calcular_media_metrica(resultado_metrica)
+                    print("Métrica :" +metric)
+                    print("Média P :" + str(media_p) + " Média R :" + str(media_r) + " Média F : " + str(media_f))
+                else:
+                    print(self.prepare_results(metric,results['p'], results['r'], results['f']))
+            print()
 
     def __calcular_pontuacao_respostas(self):
         i = 0
@@ -323,3 +387,4 @@ class Chatbot:
         print("Interação Automatizada com Chatbot TIA")
         self.__avaliar_questoes_testes(pairs)
         self.__calcular_pontuacao_respostas()
+        self.calcular_rouge()
